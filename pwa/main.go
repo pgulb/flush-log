f9e148e21438eff49712470ee4320c658a3e668c
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-10-22 20:34:44 +0200

    basic login from pwa

--- pwa/main.go
+++ pwa/main.go
@@ -11,6 +11,10 @@ import (
 	"github.com/maxence-charriere/go-app/v10/pkg/app"
 )
 
+const (
+	yellowButtonCss = "font-bold bg-yellow-500 p-2 rounded text-white"
+)
+
 type creds struct {
 	UserColonPass string
 	LoggedIn      bool
@@ -92,16 +96,17 @@ type buttonLogin struct {
 
 func (b *buttonLogin) Render() app.UI {
 	return app.Button().Text("Log in").OnClick(b.onClick).Class(
-		"font-bold bg-yellow-500 p-2 rounded text-white")
+		yellowButtonCss)
 }
 func (b *buttonLogin) onClick(ctx app.Context, e app.Event) {
 	log.Println("Logging in...")
 	user := app.Window().GetElementByID("username").Get("value").String()
 	pass := app.Window().GetElementByID("password").Get("value").String()
 
-	if user == "admin" && pass == "admin" {
+	status, basic_auth := tryLogin(ctx, user, pass)
+	if status == 200 {
 		ctx.SetState("creds", creds{
-			UserColonPass: base64.StdEncoding.EncodeToString([]byte(user + ":" + pass)),
+			UserColonPass: basic_auth,
 			LoggedIn:      true,
 		}).ExpiresIn(time.Second * 60).PersistWithEncryption()
 		app.Window().Set("location", ".")
@@ -121,7 +126,7 @@ func (b *buttonLogout) onClick(ctx app.Context, e app.Event) {
 	app.Window().Set("location", ".")
 }
 
-func getDataFromApi(ctx app.Context) string {
+func getApiUrl() string {
 	r, err := http.Get("web/apiurl")
 	if err != nil {
 		displayError(err)
@@ -130,7 +135,34 @@ func getDataFromApi(ctx app.Context) string {
 	if err != nil {
 		displayError(err)
 	}
-	apiUrl := string(bytes)
+	return string(bytes)
+}
+
+func tryLogin(ctx app.Context, username string, password string) (int, string) {
+	apiUrl := getApiUrl()
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	if err != nil {
+		displayError(err)
+	}
+	basic_auth := base64.StdEncoding.EncodeToString(
+		[]byte(username + ":" + password),
+	)
+	req.Header.Add("Authorization", "Basic "+basic_auth)
+	r, err := http.DefaultClient.Do(req)
+	if err != nil {
+		displayError(err)
+	}
+	defer func() {
+		err := r.Body.Close()
+		if err != nil {
+			displayError(err)
+		}
+	}()
+	return r.StatusCode, basic_auth
+}
+
+func getDataFromApi(ctx app.Context) string {
+	apiUrl := getApiUrl()
 	req, err := http.NewRequest("GET", apiUrl, nil)
 	if err != nil {
 		displayError(err)
@@ -139,7 +171,7 @@ func getDataFromApi(ctx app.Context) string {
 	ctx.GetState("creds", &c)
 	req.Header.Add("Authorization", "Basic "+c.UserColonPass)
 
-	r, err = http.DefaultClient.Do(req)
+	r, err := http.DefaultClient.Do(req)
 	if r.StatusCode == 401 {
 		ctx.SetState("creds", creds{LoggedIn: false}).PersistWithEncryption()
 		app.Window().Set("location", "login")
@@ -153,7 +185,7 @@ func getDataFromApi(ctx app.Context) string {
 			displayError(err)
 		}
 	}()
-	bytes, err = io.ReadAll(r.Body)
+	bytes, err := io.ReadAll(r.Body)
 	if err != nil {
 		displayError(err)
 	}
