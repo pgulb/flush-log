9a61c2ab0713c431654779f5b21b1332afcae955
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-10-22 10:25:19 +0200

    add mocked find_one and delete_one

--- api/db.py
+++ api/db.py
@@ -3,6 +3,11 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, DuplicateKeyError
 
 
+class MockDeletedResult:
+    def __init__(self, deleted_count: int):
+        self.deleted_count = deleted_count
+
+
 class MockClient:
     def __init__(self, not_avalaible: str):
         self.admin = self
@@ -12,6 +17,8 @@ class MockClient:
         self.users = self
         if not_avalaible == "true":
             self.insert_one = self.__bad_command
+            self.delete_one = self.__bad_command
+            self.find_one = self.__bad_command
         self.users_collection = {}
 
     def __bad_command(self, command: str):
@@ -27,6 +34,17 @@ class MockClient:
             self.users_collection[document["_id"]] = document
         else:
             raise DuplicateKeyError("Document already exists")
+
+    def delete_one(self, document: dict) -> MockDeletedResult:
+        if document["_id"] in self.users_collection.keys():
+            del self.users_collection[document["_id"]]
+            return MockDeletedResult(deleted_count=1)
+        raise Exception("Document not found")
+
+    def find_one(self, document: dict) -> dict:
+        if document["_id"] in self.users_collection.keys():
+            return self.users_collection[document["_id"]]
+        raise Exception("Document not found")
 
 
 def hash_password(password: str) -> str:
