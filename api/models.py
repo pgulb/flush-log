07e55260dd80c63f2fade9e0cb700105df740e8f
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-10-22 18:25:42 +0200

    change validators

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 import re
 from datetime import datetime
 
-from pydantic import BaseModel, Field, ValidationInfo, field_validator
+from pydantic import BaseModel, ValidationInfo, field_validator
 
 
 class User(BaseModel):
-    username: str = Field(max_length=60)
-    password: str = Field(min_length=8, max_length=60)
+    username: str
+    password: str
 
     @field_validator("username")
     @classmethod
@@ -15,14 +15,23 @@ class User(BaseModel):
             raise ValueError(
                 "Use only alphanumeric characters and underscores for username"
             )
+        if len(v) > 60:  # noqa: PLR2004
+            raise ValueError("Username must be at most 60 characters long")
+        return v
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, v):
+        if len(v) < 8 or len(v) > 60:  # noqa: PLR2004
+            raise ValueError("Password must be 8-60 characters long")
         return v
 
 
 class Flush(BaseModel):
     time_start: str
     time_end: str
-    rating: int = Field(min=1, max=10)
-    note: str = Field(min_length=0, max_length=100)
+    rating: int
+    note: str
     phone_used: bool
 
     @field_validator("time_end")
@@ -39,4 +48,18 @@ class Flush(BaseModel):
             datetime.fromisoformat(v)
         except Exception as e:
             raise ValueError("Invalid datetime format") from e
+        return v
+
+    @field_validator("rating")
+    @classmethod
+    def validate_rating(cls, v):
+        if v < 1 or v > 10:  # noqa: PLR2004
+            raise ValueError("Rating must be 1-10")
+        return v
+
+    @field_validator("note")
+    @classmethod
+    def validate_note(cls, v):
+        if len(v) > 100:  # noqa: PLR2004
+            raise ValueError("Note must be at most 100 characters")
         return v
