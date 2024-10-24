ab74445fb884719da9e4478f18abeb2637594e3a
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-10-24 14:56:30 +0200

    tests for pages loading and for error div check

--- pwa/test/integration/integration_test.go
+++ pwa/test/integration/integration_test.go
@@ -5,7 +5,16 @@ import (
 	"testing"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
 )
+
+func endpoints() []string {
+	base := os.Getenv("GOAPP_URL")
+	return []string{
+		base,
+		base + "/login",
+	}
+}
 
 func TestEnvIsSet(t *testing.T) {
 	if os.Getenv("GOAPP_URL") == "" {
@@ -13,7 +22,36 @@ func TestEnvIsSet(t *testing.T) {
 	}
 }
 
-func TestLoginPage(t *testing.T)() {
-    page := rod.New().MustConnect().MustPage(os.Getenv("GOAPP_URL"))
-    page.MustWaitStable().MustElementByJS("document.querySelector('body')")
+func TestLoadPages(t *testing.T)() {
+	for _, url := range endpoints() {
+		p, err := rod.New().MustConnect().Page(proto.TargetCreateTarget{
+			URL: url})
+		if err != nil {
+			t.Fatal(err)
+		}
+		p.MustWaitStable()
+		err = p.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestCheckForErrorDivId(t *testing.T)() {
+	for _, url := range endpoints() {
+		p, err := rod.New().MustConnect().Page(proto.TargetCreateTarget{
+			URL: url})
+		if err != nil {
+			t.Fatal(err)
+		}
+		p = p.MustWaitStable()
+		err = p.MustElement("#error").WaitInvisible()
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = p.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 }
