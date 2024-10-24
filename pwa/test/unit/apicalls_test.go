ea70e33d641e531ef954a8273394ac41ee762374
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-10-24 11:03:34 +0200

    add more pwa tests

--- pwa/test/unit/apicalls_test.go
+++ pwa/test/unit/apicalls_test.go
@@ -1,12 +1,16 @@
 package test
 
 import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/url"
 	"testing"
 
 	f "github.com/pgulb/flush-log/flush"
 )
 
-func TestGetApiUrl(t *testing.T) {
+func TestBasicAuth(t *testing.T) {
 	cases := []map[string]string{
 		{
 			"username": "test",
@@ -29,6 +33,73 @@ func TestGetApiUrl(t *testing.T) {
 		basicAuth := f.BasicAuth(c["username"], c["password"])
 		if basicAuth != c["expected"] {
 			t.Fatal("expected", c["expected"], "got", basicAuth)
+		}
+	}
+}
+
+func TestCloseBody(t *testing.T) {
+	r := &http.Response{
+		Body: io.NopCloser(&bytes.Buffer{}),
+	}
+	f.CloseBody(r)
+}
+
+func TestAuthorizedRequest(t *testing.T) {
+	cases := []map[string]interface{}{
+		{
+			"method":   "GET",
+			"url":      "http://localhost",
+			"basicAuth": f.BasicAuth("test", "test"),
+			"expected":  http.Request{
+				Method: "GET",
+				URL:    &url.URL{
+					Scheme: "http",
+					Host: "localhost",
+				},
+				Header: http.Header{"Authorization": []string{"Basic dGVzdDp0ZXN0"}},
+			},
+		},
+		{
+			"method":   "POST",
+			"url":      "https://example.com",
+			"basicAuth": f.BasicAuth("test2", "test2"),
+			"expected":  http.Request{
+				Method: "POST",
+				URL:    &url.URL{
+					Scheme: "https",
+					Host: "example.com",
+				},
+				Header: http.Header{"Authorization": []string{"Basic dGVzdDI6dGVzdDI="}},
+			},
+		},
+		{
+			"method":   "PUT",
+			"url":      "https://localhost:1234",
+			"basicAuth": f.BasicAuth("test3", "test3"),
+			"expected":  http.Request{
+				Method: "PUT",
+				URL:    &url.URL{
+					Scheme: "https",
+					Host: "localhost:1234",
+				},
+				Header: http.Header{"Authorization": []string{"Basic dGVzdDM6dGVzdDM="}},
+			},
+		},
+	}
+	for _, c := range cases {
+		req, err := f.AuthorizedRequest(
+			c["method"].(string), c["url"].(string), c["basicAuth"].(string))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if req.URL.String() != c["expected"].(http.Request).URL.String() {
+			t.Fatal("expected", c["expected"].(http.Request).URL, "got", req.URL)
+		}
+		if req.Method != c["expected"].(http.Request).Method {
+			t.Fatal("expected", c["expected"].(http.Request).Method, "got", req.Method)
+		}
+		if req.Header.Get("Authorization") != c["expected"].(http.Request).Header.Get("Authorization") {
+			t.Fatal("expected", c["expected"].(http.Request).Header.Get("Authorization"), "got", req.Header.Get("Authorization"))
 		}
 	}
 }
