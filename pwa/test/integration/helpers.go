ab80d9f92b314d25f5520c47a634bff3d11d7c67
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-10-30 16:03:26 +0100

    change MustWaitRequestIdle() to MustWaitIdle()

--- pwa/test/integration/helpers.go
+++ pwa/test/integration/helpers.go
@@ -33,7 +33,7 @@ func LoginPage() (*rod.Page, *rod.Browser) {
     u := launcher.New().Bin(LauncherSystemBrowser()).MustLaunch()
 	b := rod.New().ControlURL(u).MustConnect()
 	p := b.MustPage(os.Getenv("GOAPP_URL")+"/login")
-	p.MustWaitRequestIdle()
+	p.MustWaitIdle()
 	log.Println("return from LoginPage()")
 	return p, b
 }
