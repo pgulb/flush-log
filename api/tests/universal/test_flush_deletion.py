2185b334df5d1ef380f91b286695af26012048ef
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-11-11 19:46:57 +0100

    add tests for api deleting flush by id

--- api/tests/universal/test_flush_deletion.py
+++ api/tests/universal/test_flush_deletion.py
@@ -5,7 +5,7 @@ from fastapi import status
 from fastapi.testclient import TestClient
 from universal.helpers import create_user
 
-from api.main import app
+from api.main import app, get_flush_count
 
 client = TestClient(app)
 
@@ -68,3 +68,56 @@ def test_flush_delete_nonexistent_flush():
             ),
         )
         assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_flush_delete_byid_nonexistent_flush():
+    create_user(client, "testflushdeletenonexistent2", "testflushdeletenonexistent2")
+    for i in range(1, 5):
+        response = client.request(
+            "DELETE",
+            f"/flush/{i}",
+            auth=httpx.BasicAuth(
+                username="testflushdeletenonexistent2",
+                password="testflushdeletenonexistent2",
+            ),
+        )
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_flush_delete_byid():
+    create_user(client, "testflushdeletebyid", "testflushdeletebyid")
+    for i in range(2):
+        response = client.put(
+            "/flush",
+            json={
+                "time_start": datetime.now().isoformat(timespec="minutes"),
+                "time_end": (
+                    datetime.now() + timedelta(minutes=15 * (i + 1))
+                ).isoformat(timespec="minutes"),
+                "rating": 5,
+                "note": "",
+                "phone_used": False,
+            },
+            auth=httpx.BasicAuth(
+                username="testflushdeletebyid", password="testflushdeletebyid"
+            ),
+        )
+        assert response.status_code == status.HTTP_201_CREATED
+    assert get_flush_count("testflushdeletebyid") == 2  # noqa: PLR2004
+    flushes = client.get(
+        "/flushes",
+        auth=httpx.BasicAuth(
+            username="testflushdeletebyid", password="testflushdeletebyid"
+        ),
+    ).json()
+    for key in flushes:
+        response = client.request(
+            "DELETE",
+            f"/flush/{key['_id']}",
+            auth=httpx.BasicAuth(
+                username="testflushdeletebyid",
+                password="testflushdeletebyid",
+            ),
+        )
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert get_flush_count("testflushdeletebyid") == 0
