1d716a1f0cbe97fcdffa194933f08bae68ccde13
Piotr Gulbinowicz <piotr.gulbinowicz@imgw.pl>
2024-11-19 12:14:05 +0100

    add pwa pagination test

--- pwa/test/integration/flush_create_test.go
+++ pwa/test/integration/flush_create_test.go
@@ -6,20 +6,10 @@ import (
 )
 
 func TestCreateFlush(t *testing.T) {
-	p, b := RegisterAndGoToNew("user_flush_test", "pass_flush_test", "pass_flush_test")
+	p, b, err := CreateFlush(true, "flush_create_test", "flush_create_test", time.Now())
 	defer b.MustClose()
 	defer p.MustClose()
-	p.MustElement("#new-flush-time-start").MustInputTime(time.Now())
-	p.MustElement("#new-flush-time-end").MustInputTime(time.Now().Add(time.Hour))
-	p.MustElement("#new-flush-rating").MustInput("5")
-	p.MustElement("#new-flush-phone-used").MustClick()
-	p.MustElement("#new-flush-note").MustInput("test comment")
-	p.MustElement("#submit-flush-button").MustClick()
-	err := p.WaitStable(time.Second * 2)
 	if err != nil {
-		t.Fatal(err)
-	}
-	if err := CheckErrorDivText(p, "placeholder"); err != nil {
 		t.Fatal(err)
 	}
 }
---
+++ pwa/test/integration/flush_pagination_test.go
@@ -0,0 +1,56 @@
+package intest
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestFlushPagination(t *testing.T) {
+	now := time.Now()
+	for i := 0; i < 6; i++ {
+		register := false
+		if i == 0 {
+			register = true
+		}
+		p, b, err := CreateFlush(
+			register,
+			"flush_pagination_test",
+			"flush_pagination_test",
+			now.Add(time.Duration(i)*time.Hour),
+		)
+		defer b.MustClose()
+		defer p.MustClose()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	p, b := Login("flush_pagination_test", "flush_pagination_test")
+	defer b.MustClose()
+	defer p.MustClose()
+	els := p.MustElements(".remove-flush-buttonz-div")
+	log.Println("number of flushes: ", len(els))
+	if len(els) != 3 {
+		t.Fatal("wrong number of flushes")
+	}
+	p.MustElement("#update-button").MustClick()
+	err := p.WaitStable(time.Second * 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	els = p.MustElements(".remove-flush-buttonz-div")
+	log.Println("number of flushes: ", len(els))
+	if len(els) != 3 {
+		t.Fatal("wrong number of flushes")
+	}
+	p.MustElement("#update-button").MustClick()
+	err = p.WaitStable(time.Second * 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	els = p.MustElements(".remove-flush-buttonz-div")
+	log.Println("number of flushes: ", len(els))
+	if len(els) != 0 {
+		t.Fatal("wrong number of flushes")
+	}
+}
